f89b0907610e5da89914addb4236e1e254e3c2f0
Rahul-budhan <rahulbudhan22@gmail.com>
2022-08-12 23:44:33 +0530

    commit2 -added the functinality to be able to store a main value and make changes to it, also to use operator and numeric value together

--- src/calci.java
+++ src/calci.java
@@ -1,33 +1,107 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class calci {
-    public double addition(double a, double b){
-     return a+b;
+    double mainValue,answer;
+    public void throwError(calci object){
+        System.out.println("ERROR");
+        object.cli();
     }
-    public double subtraction(double a,double b){
-        return a-b;
+    public double addition(double a){
+        return mainValue+a;
     }
-    public double multiplication(double a, double b){
-        return a*b;
+    public double subtraction(double a){
+        return mainValue-a;
     }
-    public double division(double a, double b){
-        return a/b;
+    public double multiplication(double a){
+        return a*mainValue;
     }
-    public double remainder(double a, double b){
-        return a%b;
+    public double division(double a){
+        return mainValue/a;
+    }
+    public double remainder(double a){
+        return mainValue%a;
     }
     public double logOf(double a){
         return Math.log(a);
     }
+    public double squareOf(double a){
+        mainValue = (a*a);
+        return mainValue;
+    }
+    public double squareRootOf(double a){
+        mainValue = Math.sqrt(a);
+        return mainValue;
+    }
+    public double powerOf(double a, int b){
+        mainValue = Math.pow(a,b);
+        return mainValue;
+    }
+    public void operatorSelect(Scanner sc, calci object){
+        System.out.println("Main Value is "+mainValue);
+        System.out.println("Enter the operator");
+        String operator = sc.next().trim();
+        if(false){
+            //check if it starts with a number, if it does then that becoomes mainValue
+        }
+        else if (operator.startsWith("+")) {
+            System.out.println("Enter the values");
+            answer = object.addition(Double.parseDouble(operator.substring(1)));
+        } else if (operator.startsWith("-")) {
+            System.out.println("Enter the values");
+            answer = object.subtraction(Double.parseDouble(operator.substring(1)));
+        } else if (operator.startsWith("/")) {
+            System.out.println("Enter the values");
+            answer = object.division(Double.parseDouble(operator.substring(1)));
+        } else if (operator.startsWith("*")) {
+            System.out.println("Enter the values");
+            answer = object.multiplication(Double.parseDouble(operator.substring(1)));
+        } else if (operator.startsWith("%")) {
+            System.out.println("Enter the values");
+            answer = object.remainder(Double.parseDouble(operator.substring(1)));
+        } else if (operator.startsWith("log")) {
+            System.out.println("Enter the value");
+            answer = object.logOf(Double.parseDouble(operator.substring(3)));
+        } else if (operator.startsWith("square")) {
+            System.out.println("Enter the value"+operator.substring(6));
+            answer = object.squareOf(Double.parseDouble(operator.substring(6)));
+        } else if (operator.startsWith("sqrt")) {
+            System.out.println("Enter the values"+(operator.substring(4)).trim());
+
+//            answer = object.squareRootOf(Double.parseDouble(operator.substring(4).trim()));
+        } else if (operator.startsWith("power")) {
+            System.out.println("Enter the values");
+            answer = object.powerOf(sc.nextDouble(), sc.nextInt());
+        } else {
+            object.throwError(object);
+        }
+        {
+            mainValue = answer;
+        }
+        System.out.println(answer);
+        object.operatorSelect(sc,object);
+    }
+    public static void clearScreen(){//Clears Screen in java
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        } catch (IOException | InterruptedException ignored) {}
+    }
     public void home(){
-        //insert clear screen function
-        System.out.println("CALCULATOR HOME");
+        System.out.println("Use this calculator to get ");
+        System.out.println("+,-,*,/,%,log,square,sqrt(square root)");
+        System.out.println("Default value is 0");
+        mainValue = 0;
     }
     public void cli(){
         Scanner sc = new Scanner(System.in);
         calci object = new calci();
-        object.
+        clearScreen();
         object.home();
+        object.operatorSelect(sc,object);
+
     }
     public static void main(String[] args) {
         calci object = new calci();
